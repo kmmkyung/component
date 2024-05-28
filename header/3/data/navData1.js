4ef622dd3a09bc9cdc5100ac5a820bbe42336407
Min Kyung <119033846+kmmkyung@users.noreply.github.com>
2024-05-28 17:46:30 +0900

    s

---
+++ header/3/data/navData1.js
@@ -0,0 +1,11 @@
+const navData1 = {
+  'Sunglasses':['All','Acetate','Metal','Combination'],
+  'Eyeglasses':['All','Acetate','Metal','Combination','Junior'],
+  'Acc':['All','CARIN Forest','Self-Care','Accessory'],
+  'Collaboration':['OIOICOLLECTION','Ollot']
+}
+
+export default navData1;
+
+
+
---
+++ header/3/data/navData2.js
@@ -0,0 +1,10 @@
+const navData2 = {
+  'Vibe on':['Journal'],
+  'Archive':['Season Campaign','Project'],
+  'Community':['Notice','Review','Q&A','파트너스 매장찾기'],
+}
+
+export default navData2;
+
+
+
